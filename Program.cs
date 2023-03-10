6aa57527d7078e8454d1421773b5d54d876b8ecb
DAAN <daanmgneni@gmail.com>
2023-03-11 01:45:03 +0530

    Added Display Message

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using LineComparison;
+
+namespace LineComputation
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }   
+
+    }
+
+}
